592aa7bd4ab58e900777402c8f0ee45aeeff9445
jtmp2r <jtmp2r@gmail.com>
2015-12-11 22:30:14 -0600

    basic skills completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
 var Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+
+document.getElementById('vanilla_output').innerHTML = "This content generated with Vanilla_JavaScript"
+$("#jquery_output").html('This content generated with jQuery');
