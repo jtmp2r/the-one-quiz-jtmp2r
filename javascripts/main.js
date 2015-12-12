7f2a47116587a8578597980ecf6f23d71e1ee0f7
jtmp2r <jtmp2r@gmail.com>
2015-12-11 22:49:35 -0600

    functions and operators

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,33 @@
 var Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 
+
+// Basic part:
+
 document.getElementById('vanilla_output').innerHTML = "This content generated with Vanilla_JavaScript"
+
 $("#jquery_output").html('This content generated with jQuery');
+
+//Functions and Operators:
+
+var a = 10;
+var b = 3;
+
+function modulus(a, b) {
+	return a % b;
+}
+
+function doMath(num1, num2, math) {
+	return math(numb, num2);
+}
+
+function add(num1, num2) {
+	console.log(num1 + num2);
+}
+function subtract(num1, num2) {
+	console.log(num1 - num2);
+}
+
+doMath(10, 5, add);
+
+doMath(10, 5, subtract);
+
